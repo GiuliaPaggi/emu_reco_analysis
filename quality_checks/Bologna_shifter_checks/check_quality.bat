9244220da8f077831edddd204d7fa6474b809cbf
Giulia Paggi <79851597+GiuliaPaggi@users.noreply.github.com>
2023-10-23 10:04:19 +0200

    Decide to backup on nas or not

--- quality_checks/Bologna_shifter_checks/check_quality.bat
+++ quality_checks/Bologna_shifter_checks/check_quality.bat
@@ -1,12 +1,14 @@
 @echo off
 
-echo ---------------------- Starting quality check ----------------------
-
-
-if "%~1"=="" (
-    echo No parameters specified. USAGE: check_quality.bat brick_number plate_number
+if "%~2"=="" (
+    echo Not all parameters specified. USAGE: check_quality.bat brick_number plate_number
     exit /B -1
 )
+
+echo ---------------------- Starting quality check ----------------------
+echo.
+echo ********************** Do not open PAVICOM yet **********************
+echo.
 
 Set Initialdir=%cd%
 Set Dir=D:\RUN1_W5_B%1\P%2
@@ -20,9 +22,15 @@ if not exist %Dir%\ (
     exit /B -1
 )
 
+if exist %NasDir%\ (
+    echo %NasDir% exist, check brick number and plate number
+    exit /B -1
+)
+
 echo Entering %Dir%
 cd /D %Dir%
 echo pwd: %cd%
+echo. 
 
 if not exist %Dir%\%LogDir%\ (
     echo Copying log files
@@ -36,7 +44,7 @@ if not exist %Dir%\%LogDir%\ (
     xcopy %PavDir%\PAVICOM.cfg %Dir%\%LogDir%\
 )
 echo.
-echo ---------------------- ORA SI PUO' APRIRE PAVICOM PER MUOVERE LO STAGE ----------------------
+echo ********************** Now you can open PAVICOM and move the stage **********************
 echo.
 
 if not exist tracks.obx (
@@ -48,32 +56,89 @@ if not exist tracks.raw.root (
     echo.
     echo ---------------------- Converting .obx to .raw.root ----------------------
     C:\LASSO_X64\win32\EdbConv.exe -mode:OPERA_MT tracks.obx
+) else (
+    echo. 
+    echo Conversion already done
 )
 
 if not exist cz.png (
     echo. 
     echo ---------------------- Doing quality plots ----------------------
     root -q -b tracks.raw.root check_raw.C
+) else (
+    echo.
+    echo Quality plots already saved
 )
 
 if not exist thickness.png (
     echo.
     echo ---------------------- Checking thickness ----------------------
     root -q -l tracks.raw.root thickness.C 
-)
-
-
-
-echo ---------------------- Backing up on NAS ----------------------
-
-if exist %NasDir%\ (
-    echo This folder already exist on the NAS
 ) else (
-    echo Creating folder on NAS
-    mkdir %NasDir%\
+    echo.
+    echo Thickness plot already saved
 )
 
-xcopy %Dir%\ %NasDir%\ /D
+:question
+echo ---------------------- Evaluate data. Do you want to back up on NAS? (y/n) ----------------------
+
+
+
+set /p dobackup=
+IF /i "%dobackup%"=="y" goto yes
+IF /i "%dobackup%"=="n" goto no
+
+echo not valid
+goto question
+
+:yes 
+
+    echo ---------------------- Backing up on NAS ----------------------
+    
+    if exist %NasDir%\ (
+        echo This folder already exist on the NAS
+    ) else (
+        echo Creating folder on NAS
+        mkdir %NasDir%\
+    )
+    
+    xcopy %Dir%\ %NasDir%\ /D
+    
+goto e
+
+
+:no 
+echo ---------------------- NOT Backing up on NAS ----------------------
+echo.
+echo ---------------------- Renaming folder ----------------------
+
+Set ErrorDir=%Dir%_scanError
+
+if not exist "%ErrorDir%" (   
+    echo No previous scan error
+    cd ..
+    move "%Dir%" "%ErrorDir%"
+    goto e
+) else (
+    goto multipledir
+)
+
+
+:multipledir
+echo There is a previous scan error. Please set the number e.g. 2 ^= ^_scanError2
+
+set /p errornumber=
+set newdir=%ErrorDir%%errornumber%
+echo %newdir%
+if exist "%newdir%" ( goto multipledir ) else (
+cd ..
+move "%Dir%" "%newdir%"
+goto e
+)
+
+
+
+:e
     
 cd /D %Initialdir%
 
